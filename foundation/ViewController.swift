0ca73a1f522960c0cd130322fb575bfc1a69e1b7
Jordi Puigdellívol <jordi@gloobus.net>
2019-09-20 18:42:50 +0200

    Initial Commit

---
+++ foundation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foundation
+//
+//  Created by Jordi Puigdellívol on 20/09/2019.
+//  Copyright © 2019 Revo Systems S.L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
