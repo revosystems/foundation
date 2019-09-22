5f33fd7be25db029fcf8810dfdb459538875d1ad
Jordi Puigdellívol <jordi@gloobus.net>
2019-09-22 16:37:17 +0200

    Working in catalina too

--- foundation/ViewController.swift
+++ foundation/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  foundation
 //
-//  Created by Jordi Puigdellívol on 20/09/2019.
-//  Copyright © 2019 Revo Systems S.L. All rights reserved.
+//  Created by Jordi Puigdellívol on 22/09/2019.
+//  Copyright © 2019 Revo Systems. All rights reserved.
 //
 
 import UIKit
