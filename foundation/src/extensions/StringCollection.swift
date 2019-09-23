5fbeff7819623052fbb84929cc3dcab7ebcc1a9a
Pau Benet Prat <pau@revo.works>
2019-09-23 17:13:55 +0200

    adding replace and trim

--- foundation/src/extensions/StringCollection.swift
+++ foundation/src/extensions/StringCollection.swift
@@ -1,14 +1,14 @@
 import Foundation
 
 extension String{
-    
+
     public func match(_ regex: String) -> [[String]] {
         let nsString = self as NSString
         return (try? NSRegularExpression(pattern: regex, options: []))?.matches(in: self, options: [], range: NSMakeRange(0, count)).map { match in
             (0..<match.numberOfRanges).map { match.range(at: $0).location == NSNotFound ? "" : nsString.substring(with: match.range(at: $0)) }
-            } ?? []
+        } ?? []
     }
-    
+
     public func toDict() -> [String: Any]? {
         if let data = self.data(using: .utf8) {
             do {
@@ -19,7 +19,7 @@ extension String{
         }
         return nil
     }
-    
+
     public func fromBase64() -> String? {
         guard let data = Data(base64Encoded: self) else {
             return nil
@@ -38,5 +38,13 @@ extension String{
             return theJSONText ?? ""
         }
         return ""
+    }
+
+    public func replace(_ what:String, _ with:String) -> String {
+        return self.replacingOccurrences(of: what, with: with)
+    }
+
+    public func trim(_ what:String = " ") -> String {
+        return self.trimmingCharacters(in: CharacterSet(charactersIn: what))
     }
 }
