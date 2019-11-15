5e1c5d4da03bfd3c78b0c2252ce4cfebc7c94edc
Jordi Puigdellívol <jordi@gloobus.net>
2019-11-15 16:20:36 +0100

    Added sound numer of loops

--- foundation/src/services/Sound.swift
+++ foundation/src/services/Sound.swift
@@ -5,15 +5,21 @@ import AVFoundation
 
     static var player : AVAudioPlayer?
 
-    @objc public class func play(_ filename: String) {
+    @objc public class func play(_ filename: String, numberOfLoops:Int = 1) {
         guard let path = Bundle.main.path(forResource:filename, ofType:"mp3") else { return }
         let file = URL(fileURLWithPath: path)
         do {
             player = try AVAudioPlayer(contentsOf: file)
+            player?.numberOfLoops = numberOfLoops
             player?.prepareToPlay()
             player?.play()
         } catch {
             debugPrint("Can't play sound \(filename): \(error)")
         }
     }
+    
+    @objc public class func stop() {
+        player?.stop()
+    }
+    
 }
