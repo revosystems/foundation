1a8946ea9013565658440b1ae048e23d0f71c3ea
Jordi Puigdellívol <jordi.p@revo.works>
2020-02-27 21:32:15 +0100

    Improve cache

--- foundation/src/services/Cache.swift
+++ foundation/src/services/Cache.swift
@@ -7,7 +7,7 @@ import Foundation
      */
     @objc @discardableResult public static func put(_ key:String, value:Any, validUntil: Date) -> Any{
         let cacheValue = ["value" : value, "validUntil" : validUntil];
-        UserDefaults.standard.set(cacheValue, forKey:self.cacheKey(key))
+        UserDefaults.standard.set(cacheValue, forKey: cacheKey(key))
         return value
     }
     
@@ -15,14 +15,14 @@ import Foundation
      Stores the value for @validForSeconds, it replaces the value if it already exists
      */
     @objc @discardableResult public static func put(_ key:String, value:Any, validForSeconds: Int) -> Any {
-        return self.put(key, value:value, validUntil:Date().addingTimeInterval(TimeInterval(validForSeconds)))
+        put(key, value:value, validUntil:Date().addingTimeInterval(TimeInterval(validForSeconds)))
     }
     /**
      The add method will only add the item to the cache if it does not already exist in the cache store. The method will return true if the item is actually added to the cache. Otherwise, the method will return false:
      */
     static public func add(_ key: String, value:Any, validUntil: Date) {
-        if (self.has(key)) { return }
-        self.put(key, value:value, validUntil: validUntil)
+        if (has(key)) { return }
+        put(key, value:value, validUntil: validUntil)
     }
     
     /**
@@ -30,7 +30,7 @@ import Foundation
      Sometimes you may wish to retrieve an item from the cache, but also store a default value if the requested item doesn't exist. For example, you may wish to retrieve all users from the cache or, if they don't exist, retrieve them from the database and add them to the cache. You may do this using the Cache::remember method:
      */
     @objc @discardableResult static public func remember(_ key:String, seconds:Int, whatToRemember:() -> Any) -> Any{
-        return self.remember(key, date: Date().addingTimeInterval(TimeInterval(seconds)), whatToRemember: whatToRemember)
+        remember(key, date: Date().addingTimeInterval(TimeInterval(seconds)), whatToRemember: whatToRemember)
     }
     
     /**
@@ -38,12 +38,12 @@ import Foundation
      Sometimes you may wish to retrieve an item from the cache, but also store a default value if the requested item doesn't exist. For example, you may wish to retrieve all users from the cache or, if they don't exist, retrieve them from the database and add them to the cache. You may do this using the Cache::remember method:
      */
     @objc @discardableResult static public func remember(_ key:String, date:Date, whatToRemember:() -> Any) -> Any{
-        if let value = self.get(key) {
+        if let value = get(key) {
             return value
         }
         
         let value = whatToRemember();
-        self.put(key, value:value, validUntil: date)
+        put(key, value:value, validUntil: date)
         return value
     }
     
@@ -51,12 +51,12 @@ import Foundation
      The get method on the Cache facade is used to retrieve items from the cache. If the item does not exist in the cache, null will be returned. If you wish, you may pass a second argument to the get method specifying the default value you wish to be returned if the item doesn't exist:
      */
     @objc public static func get(_ key: String) -> Any? {
-        guard let cachedValue: [String: Any] = UserDefaults.standard.value(forKey: self.cacheKey(key)) as? [String: Any] else {
+        guard let cachedValue: [String: Any] = UserDefaults.standard.value(forKey: cacheKey(key)) as? [String: Any] else {
             return nil
         }
         let validUntil: Date = cachedValue["validUntil"] as! Date;
         if Date() > validUntil {
-            self.forget(key)
+            forget(key)
             return nil
         }
         return cachedValue["value"]!
@@ -66,23 +66,23 @@ import Foundation
      The get method on the Cache facade is used to retrieve items from the cache. If the item does not exist in the cache, null will be returned. If you wish, you may pass a second argument to the get method specifying the default value you wish to be returned if the item doesn't exist:
      */
     @objc static public func get(_ key: String, defaultValue:Any) -> Any? {
-        return self.get(key) ?? defaultValue;
+        get(key) ?? defaultValue;
     }
     
     /**
      The get method on the Cache facade is used to retrieve items from the cache. If the item does not exist in the cache, null will be returned. If you wish, you may pass a second argument to the get method specifying the default value you wish to be returned if the item doesn't exist:
      */
     static public func get(_ key: String, defaultValue:() -> Any ) -> Any{
-        return self.get(key) ?? defaultValue()
+        get(key) ?? defaultValue()
     }
     
     /**
      If you need to retrieve an item from the cache and then delete the item, you may use the pull method. Like the get method, null will be returned if the item does not exist in the cache:
      */
     static public func pull(_ key : String) -> Any?{
-        let value = self.get(key);
+        let value = get(key);
         if value != nil {
-            self.forget(key)
+            forget(key)
         }
         return value
     }
@@ -91,17 +91,17 @@ import Foundation
      The has method may be used to determine if an item exists in the cache. This method will return false if the value is null:
      */
     static public func has(_ key : String) -> Bool{
-        return UserDefaults.standard.value(forKey: self.cacheKey(key))  != nil;
+        UserDefaults.standard.value(forKey: cacheKey(key))  != nil;
     }
     
     /**
      You may remove items from the cache using the forget method:
      */
     @objc static public func forget(_ key : String){
-        UserDefaults.standard.removeObject(forKey: self.cacheKey(key))
+        UserDefaults.standard.removeObject(forKey: cacheKey(key))
     }
     
     private static func cacheKey(_ key:String) -> String{
-        return "cache-\(key)"
+        "cache-\(key)"
     }
 }
