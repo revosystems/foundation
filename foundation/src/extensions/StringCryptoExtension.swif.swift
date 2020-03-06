dd235c5249d14ffb278088e693a7fa50eb06c645
Jordi Puigdellívol <jordi.p@revo.works>
2020-03-06 13:19:26 +0100

    Version updated

---
+++ foundation/src/extensions/StringCryptoExtension.swif.swift
@@ -0,0 +1,16 @@
+import Foundation
+import CommonCrypto
+
+extension String {
+    
+    var sha256: String {
+        let data = Data(utf8)
+        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
+
+        data.withUnsafeBytes { buffer in
+            _ = CC_SHA256(buffer.baseAddress, CC_LONG(buffer.count), &hash)
+        }
+
+        return hash.map { String(format: "%02hhx", $0) }.joined()
+    }
+}
