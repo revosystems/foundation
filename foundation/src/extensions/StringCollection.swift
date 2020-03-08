9234a65a45ba4d4685a93b76c13dc05a339cad51
Jordi Puigdellívol <jordi.p@revo.works>
2020-03-08 16:33:28 +0100

    Adding lots of string helpers

--- foundation/src/extensions/StringCollection.swift
+++ foundation/src/extensions/StringCollection.swift
@@ -51,10 +51,36 @@ extension String{
     public func replace(_ what:String, _ with:String) -> String {
         replacingOccurrences(of: what, with: with)
     }
+    
+    public func replaceFirst(_ what:String, _ with:String) -> String {
+        if let range = range(of: what) {
+            var newString = self
+            newString.replaceSubrange(range, with: with)
+            return newString
+        }
+        return self
+    }
 
+    /**
+     The trim method trims the given string:
+     */
     public func trim(_ what:String = " ") -> String {
         trimmingCharacters(in: CharacterSet(charactersIn: what))
     }
+    
+    /**
+     The trim method trims the given string just the left side
+     */
+    /*public func ltrim(_ what:String = " ") -> String {
+        trimmingCharacters(in: CharacterSet(charactersIn: what))
+    }*/
+    
+    /**
+     The trim method trims the given string, just the right side
+     */
+    /*public func rtrim(_ what:String = " ") -> String {
+        trimmingCharacters(in: CharacterSet(charactersIn: what))
+    }*/
     
     public func lpad(toLength:Int, withPad:String = " ") -> String {
         let size = toLength - self.count
@@ -65,12 +91,130 @@ extension String{
         self.padding(toLength: toLength, withPad: withPad, startingAt: 0)
     }
     
+    /*
+     The limit method truncates the given string at the specified length:
+     */
     public func limit(_ limit:Int, ending:String = "") -> String {
         guard count > limit else { return self }
         return prefix(limit) + ending
     }
     
+    /**
+     The explode method splits the string by the given delimiter and returns a collection containing each section of the split string:
+     */
+    public func explode(_ separator:String) -> [String] {
+        components(separatedBy: separator)
+    }
+    
     public func ucFirst() -> String {
       prefix(1).uppercased() + dropFirst()
+    }
+    
+    public func lcFirst() -> String {
+      prefix(1).lowercased() + dropFirst()
+    }
+    
+    public func ucWords(_ separator:String = " ") -> String{
+        explode(separator).map { $0.ucFirst() }.implode(separator)
+    }
+    
+    /**
+     The after method returns everything after the given value in a string. The entire string will be returned if the value does not exist within the string:
+     */
+    public func after(_ what:String) -> String {
+        guard let found = range(of: what) else { return self }
+        let start = index(found.upperBound, offsetBy: 0)
+        return String(self[start...])
+    }
+    
+    /**
+     The before method returns everything before the given value in a string:
+     */
+    public func before(_ what:String) -> String {
+        guard let found = range(of: what) else { return self }
+        let start = index(found.lowerBound, offsetBy: -1)
+        return String(self[...start])
+    }
+    
+    /**
+     The Str::containsAll method determines if the given string contains all array values:
+     */
+    public func containsAll(_ what:[String]) -> Bool{
+        what.allSatisfy { self.contains($0) }
+    }
+    
+    // MARK: Start / End
+    
+    /**
+     The startsWith method determines if the given string begins with the given value:
+     */
+    public func startsWith(_ what:String) -> Bool{
+        hasPrefix(what)
+    }
+    
+    /**
+     The Str::endsWith method determines if the given string ends with the given value:
+     */
+    public func endsWith(_ what:String) -> Bool{
+        hasSuffix(what)
+    }
+    
+    
+    /**
+     The Str::finish method adds a single instance of the given value to a string if it does not already end with the value:
+     */
+    public func finish(_ with:String) -> String {
+        if endsWith(with) { return self }
+        return self + with
+    }
+    
+    /**
+     The Str::start method adds a single instance of the given value to a string if it does not already start with the value:
+     */
+    public func start(_ with:String) -> String {
+        if startsWith(with) { return self }
+        return with + self
+    }
+    
+    public func prepend(_ with:String) -> String {
+        with + self
+    }
+    
+    public func append(_ with:String) -> String {
+        self + with
+    }
+    
+    // MARK: Case types    
+    /**
+     The studly method converts the given string to StudlyCase: foo_bar => FooBar
+     */
+    public func studly() -> String {
+        explode("_").map { $0.ucFirst() }.implode("")
+    }
+    
+    /**
+    The Str::camel method converts the given string to camelCase: foo_bar => fooBar
+    */
+    public func camel() -> String {
+       studly().lcFirst()
+    }
+    
+    /**
+     The snake method converts the given string to snake_case: fooBar => foo_bar
+     https://gist.github.com/dmsl1805/ad9a14b127d0409cf9621dc13d237457
+     */
+    public func snake(_ glue:String = "_") -> String {
+        let pattern = "([a-z0-9])([A-Z])"
+
+        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        let range = NSRange(location: 0, length: count)
+        return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "$1\(glue)$2").lowercased() ?? self
+    }
+
+    /**
+    The kebab method converts the given string to kebab-case: kebabKase => kebab-case
+    */
+    public func kebab() -> String {
+        snake("-")
     }
 }
--- foundationTests/StringCollectionTest.swift
+++ foundationTests/StringCollectionTest.swift
@@ -8,6 +8,8 @@
 
 import XCTest
 
+// https://laravel.com/docs/7.x/helpers#method-fluent-str-limit
+
 @testable import RevoFoundation
 
 class StringCollectionTest: XCTestCase {
@@ -28,6 +30,126 @@ class StringCollectionTest: XCTestCase {
         let result = string.rpad(toLength:10, withPad:"0")
         
         XCTAssertEqual("abc0000000", result)
+    }
+    
+    func test_explode(){
+        let result = "abc:def:jk".explode(":")
+        XCTAssertEqual(["abc", "def", "jk"], result)
+    }
+    
+    func test_uc_first(){
+        XCTAssertEqual("HolA", "holA".ucFirst())
+    }
+    
+    func test_lc_first(){
+        XCTAssertEqual("holA", "HolA".lcFirst())
+    }
+    
+    func test_uc_words(){
+        XCTAssertEqual("My Little TowN", "my little towN".ucWords())
+        XCTAssertEqual("My-Little-TowN", "my-little-towN".ucWords("-"))
+    }
+    
+    func test_after(){
+        XCTAssertEqual("this is my name", "this is my name".after("not in the string"))
+        XCTAssertEqual(" my name", "this is my name".after("this is"))
+    }
+    
+    func test_before(){
+        XCTAssertEqual("this is my name", "this is my name".before("not in the string"))
+        XCTAssertEqual("this is ", "this is my name".before("my name"))
+    }
+    
+    func test_trim(){
+        XCTAssertEqual("hello", "  hello  ".trim() )
+        XCTAssertEqual("hello", "...hello...".trim(".") )
+    }
+    
+    /*func test_ltrim(){
+        XCTAssertEqual("hello   ", "  hello  ".ltrim() )
+        XCTAssertEqual("hello...", "...hello...".ltrim(".") )
+    }
+    
+    func test_rtrim(){
+        XCTAssertEqual("   hello", "  hello  ".rtrim() )
+        XCTAssertEqual("...hello", "...hello...".rtrim(".") )
+    }*/
+    
+    func test_studly(){
+        XCTAssertEqual("FooBar", "foo_bar".camel())
+    }
+    
+    func test_camel(){
+        XCTAssertEqual("fooBar", "foo_bar".camel())
+    }
+    
+    func test_snake(){
+        XCTAssertEqual("foo_bar", "fooBar".snake())
+    }
+    
+    func test_kebab(){
+        XCTAssertEqual("foo-bar", "fooBar".kebab())
+    }
+    
+    func test_contains_all(){
+        XCTAssertTrue("my name".containsAll(["my", "name"]))
+        XCTAssertFalse("my name".containsAll(["my", "name", "is"]))
+    }
+    
+    func test_starts_with(){
+        XCTAssertTrue("my potateo".startsWith("my"))
+        XCTAssertFalse("my potateo".startsWith("potateo"))
+    }
+    
+    func test_ends_with(){
+        XCTAssertFalse("my potateo".endsWith("my"))
+        XCTAssertTrue("my potateo".endsWith("potateo"))
+    }
+    
+    func test_finish(){
+        XCTAssertEqual("route/", "route".finish("/"))
+        XCTAssertEqual("route/", "route/".finish("/"))
+    }
+    
+    func test_start(){
+        XCTAssertEqual("/route", "route".start("/"))
+        XCTAssertEqual("/route/", "/route/".start("/"))
+    }
+    
+    func test_prepend(){
+        XCTAssertEqual("my name", "name".prepend("my "))
+    }
+    
+    func test_append(){
+        XCTAssertEqual("my name", "my".append(" name"))
+    }
+    
+    func test_replace(){
+        XCTAssertEqual("my other name", "my real name".replace("real", "other"))
+    }
+    
+    func test_replace_first(){
+        XCTAssertEqual("my other real name", "my real real name".replaceFirst("real", "other"))
+    }
+    
+    func test_replace_matches(){
+        
+    }
+    
+    func test_substr(){
+        
+    }
+    
+    func test_when_empty(){
+        
+    }
+    
+    func test_words(){
+        
+    }
+    
+    func test_limit(){
+        
     }
 
 }
