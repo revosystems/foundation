a686f0bf540ab7751e6cfca2b4aebd19fbb1dcca
Jordi Puigdellívol <jordi@gloobus.net>
2020-05-20 13:14:28 +0200

    Removed redundant public

--- foundation/src/extensions/StringSubscripts.swift
+++ foundation/src/extensions/StringSubscripts.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 public extension String {
-  public subscript(_ i: Int) -> String {
+  subscript(_ i: Int) -> String {
     let idx1 = index(startIndex, offsetBy: i)
     let idx2 = index(idx1, offsetBy: 1)
     return String(self[idx1..<idx2])
   }
 
-  public subscript (r: Range<Int>) -> String {
+  subscript (r: Range<Int>) -> String {
     let start = index(startIndex, offsetBy: r.lowerBound)
     let end = index(startIndex, offsetBy: r.upperBound)
     return String(self[start ..< end])
   }
 
-  public subscript (r: CountableClosedRange<Int>) -> String {
+  subscript (r: CountableClosedRange<Int>) -> String {
     let startIndex =  self.index(self.startIndex, offsetBy: r.lowerBound)
     let endIndex = self.index(startIndex, offsetBy: r.upperBound - r.lowerBound)
     return String(self[startIndex...endIndex])
