82b5585388516163055322207b034f5d150ab68f
aleixgilaguilar <aleix.gil@revo.works>
2020-09-04 11:31:45 +0200

    String Range method added.

--- foundation/src/extensions/StringCollection.swift
+++ foundation/src/extensions/StringCollection.swift
@@ -255,4 +255,12 @@ extension String{
     public func kebab() -> String {
         snake("-")
     }
+    
+    public func ranges(of substring: String, options: CompareOptions = [], locale: Locale? = nil) -> [Range<Index>] {
+        var ranges: [Range<Index>] = []
+        while let range = range(of: substring, options: options, range: (ranges.last?.upperBound ?? self.startIndex)..<self.endIndex, locale: locale) {
+            ranges.append(range)
+        }
+        return ranges
+    }
 }
