c0548d456002b452055ad26afc0dcf057c3bd6ec
Jordi Puigdellívol <jordi.p@revo.works>
2020-11-03 11:06:26 +0100

    Made public

--- foundation/src/extensions/DataExtension.swift
+++ foundation/src/extensions/DataExtension.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Data {
-    var toJson: String? {
+    public var toJson: String? {
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
               let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
               let prettyPrintedString = String(data: data, encoding:.utf8) else { return nil }
