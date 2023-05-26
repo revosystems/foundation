32943ba046a792178f4a3bc95c59df0aacdf7032
aleixgilaguilar <aleix.gil@revo.works>
2023-05-26 17:20:50 +0200

    Revert "UICollectionViewExtension and UIViewControllerExtension: scroll and dismiss gesture."
    This reverts commit 93f84bcf51fcef95d4a2860a4cde711c46074441.

--- foundation/src/extensions/UIViewControllerExtension.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-@objc extension UIViewController {
-    public func disableModalDismissGesture() {
-        presentationController?.presentedView?.gestureRecognizers?.first?.isEnabled = false
-    }
-}
