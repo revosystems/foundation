70bd197bec87641e5f7ccab4041c9cf541525c2d
adriamartin <adria.martin@revo.works>
2023-09-18 08:38:54 +0200

    feature: add retry with async completion.

--- foundation/src/GeneralHelpers.swift
+++ foundation/src/GeneralHelpers.swift
@@ -65,3 +65,26 @@ func retry(tries:Int = 1, delayMs:UInt32 = 0, block:@escaping(_ succeeded:@escap
     }
 }
 
+enum RetryError: Error, CustomStringConvertible {
+    case maximumRetriesReached(error:String?)
+
+    var description: String {
+        switch self {
+            case .maximumRetriesReached(let error): return error ?? "Max retries reached"
+        }
+    }
+}
+
+@discardableResult
+func retry<T>(tries:Int = 1, delayMs:UInt32 = 0, _ operation: @escaping () async throws -> T,_ currentError : String? = nil) async throws {
+    guard tries >= 0 else {
+        throw RetryError.maximumRetriesReached(error: currentError)
+    }
+    do {
+        try await operation()
+    } catch {
+        usleep(delayMs)
+        try await retry(tries:tries - 1, delayMs: delayMs, operation, "\(error)")
+    }
+}
+
