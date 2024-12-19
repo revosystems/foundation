89821de9c5b0a56f1be19cdbad6ad4c8011bc559
Sergi Baró <sergi.baro@revo.works>
2024-12-19 12:54:02 +0100

    feat: added CodableTransformer class.

---
+++ foundation/src/transformers/CodableTransformer.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+
+public class CodableTransformer<T: Codable>: ValueTransformer {
+    
+    public static func register(type: T.Type) {
+        ValueTransformer.setValueTransformer(CodableTransformer<T>(), forName: NSValueTransformerName("CodableTransformer"))
+    }
+    
+    public override func transformedValue(_ value: Any?) -> Any? {
+        guard let value = value as? T else { return nil }
+        
+        do {
+            return try JSONEncoder().encode(value)
+        } catch {
+            print("Error encoding \(T.self): \(error)")
+            return nil
+        }
+    }
+
+    public override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else { return nil }
+        
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            print("Error decoding \(T.self): \(error)")
+            return nil
+        }
+    }
+    
+}
