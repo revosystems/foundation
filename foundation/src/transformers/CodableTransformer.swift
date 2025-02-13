6fc00021f73a37f54a382e1ff301f3aff721fc88
Sergi Baró <sergi.baro@revo.works>
2025-02-13 16:56:51 +0100

    fix: set default codable transformer name.

--- foundation/src/transformers/CodableTransformer.swift
+++ foundation/src/transformers/CodableTransformer.swift
@@ -3,7 +3,7 @@ import Foundation
 
 public class CodableTransformer<T: Codable>: ValueTransformer {
     
-    public static func register(type: T.Type, name: String) {
+    public static func register(type: T.Type, name: String = "CodableTransformer") {
         ValueTransformer.setValueTransformer(CodableTransformer<T>(), forName: NSValueTransformerName(name))
     }
     
